db3247b7c731e9933671bda17aee3aa920cd0021
yuunseo <dbstj7457@naver.com>
2023-01-12 20:59:35 +0900

    #2. 조건문 part.2

--- Nomadcoder/EX1/app.js
+++ Nomadcoder/EX1/app.js
@@ -11,3 +11,13 @@ else{
     //괄호 안이 false일때, 동작
     console.log("Number");
 }
+
+if(isNaN(age)){
+    console.log("Please write a number!");
+}else if(age < 18){// 또 다른 조건 추가!
+    console.log("You are too young");
+}else if(age>=18 && age<30){
+    console.log("You are so pretty, you can drink!");
+}else{
+    console.log("You are so nice, You can drink!");
+}
