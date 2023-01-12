31eaa62403efe7b7e206da2b9cd60c3251140f21
yuunseo <dbstj7457@naver.com>
2023-01-12 15:47:26 +0900

    #2. 함수와 리턴

--- Nomadcoder/EX1/app.js
+++ Nomadcoder/EX1/app.js
@@ -1,24 +1,8 @@
-function sayHello(nameOfPerson){
-    console.log("Hello!",nameOfPerson);
+//return으로 값을 빼내주지 않으면, undefined된 값이 로그로 출력될겨
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner +2;
 }
 
-sayHello("yunseo");
-sayHello("yundong");
-sayHello("yunnam");
-sayHello("yunbook");
-
-//함수를 정의하고 사용하는 방법과
-//함수의 매개변수(인자)를 전달하는 방법
-//괄호에 값을 넣어 함수에 전달하여 실행하자.
-
-const player ={
-    name: "yunseo",
-    sayBye: function(name){
-        console.log("Bye!",name);
-    }
-};
-//객체 내에도 함수를 정의할 수 있다.
-//괄호의 사용 방식은 동일하다.
-
-console.log(player.name);
-player.sayBye(player.name);
+console.log(calculateKrAge(96));
+calculateKrAge(24);
+//즉, 리턴하는 것 != 출력
