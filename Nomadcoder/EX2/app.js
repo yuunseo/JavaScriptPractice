447c624934e2d643afe8628fe65e8e2a816dd4bc
yuunseo <dbstj7457@naver.com>
2023-01-12 21:35:14 +0900

    #3. querySelector

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -1,3 +1,4 @@
-const hi = document.getElementsByClassName("hello");
-//이번엔 클래스명으로 element 접근
-console.log(hi);
+const title = document.querySelector(".hello h1");
+console.log(title);
+//hello라는 class내의 h1태그인 element를 가져와라
+//.을 쓴 이유는 CSS selector기 때문!
