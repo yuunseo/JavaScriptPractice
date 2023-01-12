cf54d60f3040f6ef04c41222b0d61d7150cb7a7f
yuunseo <dbstj7457@naver.com>
2023-01-12 21:13:24 +0900

    #3. document 객체+

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -1,1 +1,7 @@
 console.log(document.title);
+//document 객체는 내가 접근하는 html을 가리키고 있기 때문에,
+//html에서 작성한 코드를 불러오고, html로 저장할 수 있다.
+
+document.title = "hi everyone~!";
+console.log(document.title);
+//즉, JS로 html을 변경함!
