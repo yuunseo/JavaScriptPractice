95474d3404a4ab41e1f8e37768183811cae87d6d
yuunseo <dbstj7457@naver.com>
2023-01-12 16:03:00 +0900

    이건 어디에 저장 되는걸까..

--- Nomadcoder/EX1/app.js
+++
@@ -1,8 +0,0 @@
-//return으로 값을 빼내주지 않으면, undefined된 값이 로그로 출력될겨
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner +2;
-}
-
-console.log(calculateKrAge(96));
-calculateKrAge(24);
-//즉, 리턴하는 것 != 출력
