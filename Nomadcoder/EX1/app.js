ff4e48ae97157b265ecacd396a6d7ae75cabb0a9
yuunseo <dbstj7457@naver.com>
2023-01-12 16:22:52 +0900

    #2. 프롬프트

--- Nomadcoder/EX1/app.js
+++ Nomadcoder/EX1/app.js
@@ -1,8 +1,7 @@
-//return으로 값을 빼내주지 않으면, undefined된 값이 로그로 출력될겨
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner +2;
-}
+//프롬프트
+const age = prompt("How old are you?");
+console.log(age);
 
-console.log(calculateKrAge(96));
-calculateKrAge(24);
-//즉, 리턴하는 것 != 출력
+//프롬프트로 받은 것은 string이기 때문에 재사용시, 형변환 가능.
+console.log(typeof age);
+console.log(typeof parseInt(age));
