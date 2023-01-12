aca62e396c9a82943b77009182e8819cff7d1325
yuunseo <dbstj7457@naver.com>
2023-01-12 22:17:14 +0900

    #3. Events+

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -1,6 +1,10 @@
 const title = document.querySelector(".hello h1");
-console.log(title);
-console.dir(title);
-//title의 메소드들?이 다 나옴
 
-title.style.color = "blue";
+function handleTitleClick(){
+    console.log("title was clicked!!");
+    title.style.color = "purple";
+}
+
+title.addEventListener("click",handleTitleClick);
+//이벤트 리스너를 만들어서 (이벤트 종류, 이벤트 행위)
+//두번째 인자로는 함수가 들어가는데, 실행시키는 것이 아니다. 주의하자!!!
