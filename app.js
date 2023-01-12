67ea76cb9c9947dcf274525777c4b8b19f62a451
yuunseo <dbstj7457@naver.com>
2023-01-12 14:21:17 +0900

    #2. 변수 선언에 관한 예제 코드

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 console.log(46152);
 console.log('hi my name is yunseo');
-//log란 print와 같은 역할. 괄호 속 내용을 출력해준다.
+//log란 print와 같은 역할. 괄호 속 내용을 console에서 출력해준다.
+
+const a = 5;
+//const란, 값이 변하지 않는 상수를 의미한다.
+
+console.log(a + 2);
+console.log(a * 2);
+
+const myName = "yunseo";
+//JS의 변수명은 시작 다음 단어의 첫 문자를 대문자로 쓴다.
+//likeThisName, hiMyNameIsYunseo 등 --> 카멜 케이스
+//Python에서는
+//like_this_name, hi_my_name_is_yunseo 등 --> 스네이크 케이스
+
+console.log("Hello "+myName);
+
+let b = 10;
+//let은 const와 다르게, 값을 변경할 수 있다.
+
+console.log(b);
+b = 7;
+console.log(b);
