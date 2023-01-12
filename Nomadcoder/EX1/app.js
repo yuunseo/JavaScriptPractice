0ba6edd9bd69d7cea706616a9da700ebd8c3ec86
yuunseo <dbstj7457@naver.com>
2023-01-12 14:57:20 +0900

    #2. 배열

--- Nomadcoder/EX1/app.js
+++ Nomadcoder/EX1/app.js
@@ -1,10 +1,19 @@
-const amIFat =false;
-//새로운 자료형: 불리안(true, false)
-console.log(amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
 
-const amINul = null;
-//아무것도 없음을 의미하는 널값.
-console.log(amINul);
+//하나하나 쓰고 출력하기 귀찮고 번거로움.
+//리스트, 즉 배열을 사용해서 저장해보자.
 
-let something;
-console.log(something);
+const daysOfWeek = [mon,tue,wed,thu,fri,sat];
+console.log(daysOfWeek);
+
+console.log(daysOfWeek[0]);
+//index를 이용해서 접근 할 수 있다.
+
+daysOfWeek.push("sun");
+//.push() 메소드로 배열에 값 추가하기
+console.log(daysOfWeek);
