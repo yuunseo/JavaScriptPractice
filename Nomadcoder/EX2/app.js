685315c6b089a946a9215dec18a6bc65b7044023
yuunseo <dbstj7457@naver.com>
2023-01-12 21:36:21 +0900

    #3. querySelector+

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -2,3 +2,5 @@ const title = document.querySelector(".hello h1");
 console.log(title);
 //hello라는 class내의 h1태그인 element를 가져와라
 //.을 쓴 이유는 CSS selector기 때문!
+//class명은 앞에 .으로 class라는 것을 명시해야하고
+//가져오고픈 태그명도 써줘야해!
