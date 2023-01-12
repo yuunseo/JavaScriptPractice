1619085ebee0cd4bd3eb4270016d12464e630e6a
yuunseo <dbstj7457@naver.com>
2023-01-12 20:44:46 +0900

    #2. if 조건문

--- Nomadcoder/EX1/app.js
+++ Nomadcoder/EX1/app.js
@@ -1,7 +1,13 @@
-//프롬프트
-const age = prompt("How old are you?");
-console.log(age);
+//NaN인지 아닌지 확인해보자.
+const age = parseInt(prompt("How old are you?"));
+console.log(isNaN(age)); //정수를 입력하면 fasle, 다른건 true
 
-//프롬프트로 받은 것은 string이기 때문에 재사용시, 형변환 가능.
-console.log(typeof age);
-console.log(typeof parseInt(age));
+//if 조건문
+if(isNaN(age)){
+    //괄호 안이 true일때, 동작
+    console.log("Not Number");
+}
+else{
+    //괄호 안이 false일때, 동작
+    console.log("Number");
+}
