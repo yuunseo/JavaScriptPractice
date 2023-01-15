92233c9cc4e4ae766dd04569b9775cb06df1ae73
yuunseo <dbstj7457@naver.com>
2023-01-15 13:36:16 +0900

    #3. 마우스 올려두는 이벤트

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -8,3 +8,9 @@ function handleTitleClick(){
 title.addEventListener("click",handleTitleClick);
 //이벤트 리스너를 만들어서 (이벤트 종류, 이벤트 행위)
 //두번째 인자로는 함수가 들어가는데, 실행시키는 것이 아니다. 주의하자!!!
+
+function handleMouseEnter(){
+    console.log("Mouse is here!!");
+}
+
+title.addEventListener("mouseenter",handleMouseEnter);
