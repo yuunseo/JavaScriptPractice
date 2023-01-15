86917df5605649712bc3346c6333bbf43ccf0311
yuunseo <dbstj7457@naver.com>
2023-01-15 13:41:58 +0900

    #3. 마우스 leave 이벤트

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -10,7 +10,15 @@ title.addEventListener("click",handleTitleClick);
 //두번째 인자로는 함수가 들어가는데, 실행시키는 것이 아니다. 주의하자!!!
 
 function handleMouseEnter(){
-    console.log("Mouse is here!!");
+    title.innerText = "Mouse is here!!";
 }
 
 title.addEventListener("mouseenter",handleMouseEnter);
+//마우스 올리면 여깄다고 글 작성
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is Gone!!";
+}
+
+title.addEventListener("mouseleave",handleMouseLeave);
+//마우스 치우면 여기 없다고 글 작성
