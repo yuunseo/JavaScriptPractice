b4038f5d4a6068888a88c1aa2b5f96578e34b959
yuunseo <dbstj7457@naver.com>
2023-01-18 20:54:09 +0900

    #3. title을 h1으로 변경하고, 이벤트 하나 더 추가

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -1,24 +1,31 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-function handleTitleClick(){
-    console.log("title was clicked!!");
-    title.style.color = "purple";
+function handleh1Click(){
+    console.log("h1 was clicked!!");
+    h1.style.color = "purple";
 }
 
-title.onClick = handleTitleClick();
+h1.addEventListener("click",handleh1Click);
 //이벤트 리스너를 만들어서 (이벤트 종류, 이벤트 행위)
 //두번째 인자로는 함수가 들어가는데, 실행시키는 것이 아니다. 주의하자!!!
 
 function handleMouseEnter(){
-    title.innerText = "Mouse is here!!";
+    h1.innerText = "Mouse is here!!";
 }
 
-title.onmouseenter  = handleMouseEnter());
+h1.addEventListener("mouseenter",handleMouseEnter);
 //마우스 올리면 여깄다고 글 작성
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is Gone!!";
+    h1.innerText = "Mouse is Gone!!"; 
 }
 
-title.onmouseLeave = handleMouseLeave());
+h1.addEventListener("mouseleave",handleMouseLeave);
 //마우스 치우면 여기 없다고 글 작성
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+window.addEventListener("resize",handleWindowResize);
+//크기가 바뀌면, 배경 색상이 바뀜.
