6c3c9ccc728aea458d86a71d4356b3ccac351058
yuunseo <dbstj7457@naver.com>
2023-01-18 20:45:57 +0900

    #3. 이벤트 리스너를 그냥 on이벤트 = 함수명(); 으로도 정의할 수 있음

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -5,7 +5,7 @@ function handleTitleClick(){
     title.style.color = "purple";
 }
 
-title.addEventListener("click",handleTitleClick);
+title.onClick = handleTitleClick();
 //이벤트 리스너를 만들어서 (이벤트 종류, 이벤트 행위)
 //두번째 인자로는 함수가 들어가는데, 실행시키는 것이 아니다. 주의하자!!!
 
@@ -13,12 +13,12 @@ function handleMouseEnter(){
     title.innerText = "Mouse is here!!";
 }
 
-title.addEventListener("mouseenter",handleMouseEnter);
+title.onmouseenter  = handleMouseEnter());
 //마우스 올리면 여깄다고 글 작성
 
 function handleMouseLeave(){
     title.innerText = "Mouse is Gone!!";
 }
 
-title.addEventListener("mouseleave",handleMouseLeave);
+title.onmouseLeave = handleMouseLeave());
 //마우스 치우면 여기 없다고 글 작성
