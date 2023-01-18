332a89ce5af8bf8c1d6d9ecd161fa0c8e24f28d0
yuunseo <dbstj7457@naver.com>
2023-01-18 21:30:03 +0900

    #3. 변수로 이벤트 추가

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -1,13 +1,16 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleh1Click(){
-    
-    if(h1.style.color === "purple"){
-        h1.style.color = "blue";
-    }else{
-        h1.style.color = "purple";
-    }
 
+    const currentColor = h1.style.color; //const는 수정 불가능
+    let newColor; //let은 수정 가능
+
+    if(currentColor === "purple"){
+        newColor = "blue";
+    }else{
+        newColor = "purple";
+    }
+   h1.style.color = newColor;
 }
 
 h1.addEventListener("click",handleh1Click);
