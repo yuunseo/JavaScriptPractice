9ebd0a07cc8655f3c04e952465e64d5d7b7b9ea1
yuunseo <dbstj7457@naver.com>
2023-01-19 11:01:37 +0900

    #3. 이전코드 간결

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -2,11 +2,7 @@ const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick(){
     const activeClass = "active";
-    if(h1.classList.contains(activeClass)){
-        h1.classList.remove(activeClass);
-    }else{
-        h1.classList.add(activeClass);
-    }
+    h1.classList.toggle(activeClass)
     
 }
 h1.addEventListener("click",handleTitleClick);
