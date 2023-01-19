47405ad2b3ed22964f47c733a1a1b9843f632ed5
yuunseo <dbstj7457@naver.com>
2023-01-19 11:47:13 +0900

    #4. input의 value 내용 얻기

--- Nomadcoder/EX3/app.js
+++ Nomadcoder/EX3/app.js
@@ -2,6 +2,10 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginButtonClick(){
+    console.dir(loginInput); //loginInput에 어떤 메소드가 있는지 확인
+    console.log(loginInput.value); //loginInput의 내용을 출력해줌
     console.log("You are log in!");
 }
+
+
 loginButton.addEventListener("click",onLoginButtonClick);
