239a53c64754bdbe568913cd6e576174183c0e9b
yuunseo <dbstj7457@naver.com>
2023-01-19 10:47:53 +0900

    #3. css로 스타일링 하기(클릭과 클릭 애니메이션)

--- Nomadcoder/EX2/app.js
+++ Nomadcoder/EX2/app.js
@@ -1,2 +1,11 @@
 const h1 = document.querySelector(".hello h1");
 
+function handleTitleClick(){
+    if(h1.className === "active"){
+        h1.className = "";
+    }else{
+        h1.className = "active";
+    }
+    
+}
+h1.addEventListener("click",handleTitleClick);
