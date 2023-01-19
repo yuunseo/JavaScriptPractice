afb9a0abe08d2b852b88df1f3f87ef8a6ac610b2
yuunseo <dbstj7457@naver.com>
2023-01-19 11:49:15 +0900

    #4. 내가 짜본 input의 유효성 검사 코드

--- Nomadcoder/EX3/app.js
+++ Nomadcoder/EX3/app.js
@@ -2,9 +2,13 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginButtonClick(){
-    console.dir(loginInput); //loginInput에 어떤 메소드가 있는지 확인
-    console.log(loginInput.value); //loginInput의 내용을 출력해줌
-    console.log("You are log in!");
+    if(loginInput.value){
+        console.log("Hi~",loginInput.value); 
+        console.log("You are log in!");
+    }else{
+        console.log("please write your name.");
+    }
+    
 }
 
 
