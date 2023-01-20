1cfc491e58964313e5b4af37a653e6c4c0c06a41
yuunseo <dbstj7457@naver.com>
2023-01-20 22:04:49 +0900

    #4. submit의 기본 동작을 막아보자. - 함수의 인자로 받는 이벤트 정보(event.preventDefault();)

--- Nomadcoder/EX3/app.js
+++ Nomadcoder/EX3/app.js
@@ -1,15 +1,10 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginButtonClick(){
-    if(loginInput.value){
-        console.log("Hi~",loginInput.value); 
-        console.log("You are log in!");
-    }else{
-        console.log("please write your name.");
-    }
+function onLoginSubmit(event){
+    const username = loginInput.value;
+    event.preventDefault();
+    console.log(username);
     
 }
-
-
-loginButton.addEventListener("click",onLoginButtonClick);
+loginForm.addEventListener("submit",onLoginSubmit);
