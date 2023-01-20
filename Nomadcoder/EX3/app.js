9ae5c8ccf6c3b530b910864072bdd93a7afecec9
yuunseo <dbstj7457@naver.com>
2023-01-20 22:32:51 +0900

    #4. 기본 동작을 막는 여러 방법들과 class를 이용한 화면 제어

--- Nomadcoder/EX3/app.js
+++ Nomadcoder/EX3/app.js
@@ -1,10 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     const username = loginInput.value;
-    event.preventDefault();
-    console.log(username);
+    event.preventDefault(); //form의 기본동작을 막아줌 = submit(새로고침)
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = "Hello~"+ username;
+    greeting.innerText = `Nice to meet you, ${username}!`;
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
     
 }
+function handleLinkClick(){
+    alert("clicked!");
+}
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
