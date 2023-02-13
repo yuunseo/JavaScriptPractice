ac17a9f919a7475bef7546d5d06a5db74d26b4d6
yuunseo <dbstj7457@naver.com>
2023-02-14 08:08:20 +0900

    JSX.문법 간단히

--- RunToDo/project/src/App.js
+++ RunToDo/project/src/App.js
@@ -3,11 +3,11 @@ import './App.css'; //스타일 설정해 줄 css 파일
 
 function App() {
   let title = "오늘의 하루는 !?";
-
+  let fonts = {color : 'pink', fontSize : '30px'};
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 블로그</div>
+        <div style={fonts}>개발 블로그</div>
       </div>
       <img src={logo} />
       <h4>{title}</h4>
