32946e262ffb1fe317093ac20dc2968f89b6a848
yuunseo <dbstj7457@naver.com>
2023-02-14 07:47:05 +0900

    리액트로 웹 개발하기

---
+++ RunToDo/project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css'; //스타일 설정해 줄 css 파일
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 블로그</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
