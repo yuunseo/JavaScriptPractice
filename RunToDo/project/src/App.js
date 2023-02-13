41bb345882f115a15e59bfb8af2fb5a334cd236b
yuunseo <dbstj7457@naver.com>
2023-02-14 08:01:13 +0900

    JSX. 데이터 바인딩 쉽게

--- RunToDo/project/src/App.js
+++ RunToDo/project/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css'; //스타일 설정해 줄 css 파일
 
 function App() {
+  let title = "오늘의 하루는 !?";
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 블로그</div>
       </div>
+      <img src={logo} />
+      <h4>{title}</h4>
     </div>
   );
 }
