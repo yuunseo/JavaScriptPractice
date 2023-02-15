e59aef07bc51b3ede079dd9b39cea73849fcead7
yuunseo <dbstj7457@naver.com>
2023-02-15 17:00:40 +0900

    4강. 이벤트리스터 장착-클릭

--- RunToDo/project/src/App.js
+++ RunToDo/project/src/App.js
@@ -1,19 +1,31 @@
 import logo from './logo.svg';
 import './App.css'; //스타일 설정해 줄 css 파일
+import React, {useState} from 'react';
 
 function App() {
-  let posts = "강남 고기 맛집";
-  let fonts = {color : 'pink', fontSize : '30px'};
+  let [title,setTitle] = useState(['남자 코드 추천','여자 가방 추천','서울 근교 카페 추천']);
+  let [num,setNum] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={fonts}>개발 블로그</div>
+        <div>개발 블로그</div>
       </div>
       <div className="list">
-        <h3>{posts}</h3>
+        <h3>{title[0]} <span onClick={()=>{setNum(num+1)}}>👍</span>{num}</h3>
         <p>2월 14일 발행</p>
         <hr/>
       </div>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>2월 15일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>2월 16일 발행</p>
+        <hr/>
+      </div>
+      
       
     </div>
   );
