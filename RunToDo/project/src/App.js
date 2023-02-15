c144c06a60966d040e77966b92c353afcd30a921
yuunseo <dbstj7457@naver.com>
2023-02-15 18:21:34 +0900

    생활코딩11.1 컴포넌트 만들기1_JSX 등

--- RunToDo/project/src/App.js
+++ RunToDo/project/src/App.js
@@ -1,60 +1,25 @@
 import './App.css'; //스타일 설정해 줄 css 파일
-import React, {useState} from 'react';
+import React,{Component} from 'react';
 
-function App() {
-  let [title,setTitle] = useState(['남자 코드 추천','남자 가방 추천','서울 근교 카페 추천']);
-  let [num,setNum] = useState(0);
-  function changeTitle(){
-    var newArray = [...title];
-    newArray[0] = '여자 코트 추천';
-    setTitle(newArray);
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        *:. world wide web .:*
+      </header>
+    );
   }
-
-  function changeSequan(){
-    var newArr = [...title];
-    newArr[0] = newArr[2];
-    newArr[2] = '코트 추천';
-    setTitle(newArr);
-
-  }
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 블로그</div>
-      </div>
-      <button onClick={changeTitle}>카테고리 변경</button>
-      <button onClick={changeSequan}>순서 변경</button>
-      <div className="list">
-        <h3>{title[0]} <span onClick={()=>{setNum(num+1)}}>👍</span>{num}</h3>
-        <p>2월 14일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{title[1]}</h3>
-        <p>2월 15일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{title[2]}</h3>
-        <p>2월 16일 발행</p>
-        <hr/>
-      </div>
-
-      <Modal/>
-      
-      
-    </div>
-  );
 }
 
-function Modal(){
-  return(
-    <div className="modal">
-        <h2>제목</h2>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
-  )
+class App extends Component {
+  render(){
 
+    return(
+      <div className="App">
+        <Subject />
+      </div>
+    );
+  }
 }
 export default App;
