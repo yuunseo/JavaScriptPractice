8a08c5a9ba3504e39b82ef5d11f1c47ecc6e7123
yuunseo <dbstj7457@naver.com>
2023-02-16 11:00:27 +0900

    컴포넌트 분리 후

--- RunToDo/project/src/App.js
+++ RunToDo/project/src/App.js
@@ -1,48 +1,15 @@
 import './App.css'; //스타일 설정해 줄 css 파일
 import React,{Component} from 'react';
-
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        <h1>WEB</h1>
-        *:. world wide web .:*
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperTet Markup Language.
-      </article>
-    );
-  }
-}
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 
 class App extends Component {
   render(){
 
     return(
       <div className="App">
-        <Subject />
+        <Subject title ="WEB" sub="*:. world wide web .:*"/>
         <TOC />
         <Content />
       </div>
