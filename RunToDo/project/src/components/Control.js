bded48d7f2e94fdfacc5a9be11f2784706eae217
yuunseo <dbstj7457@naver.com>
2023-02-18 09:36:14 +0900

    컴포넌트 내 핸들러 적용

--- RunToDo/project/src/components/Control.js
+++ RunToDo/project/src/components/Control.js
@@ -4,9 +4,18 @@ class Control extends Component{
     render(){
       return(
         <ul>
-          <li><a href="/create">create</a></li>
-          <li><a href="/update">update</a></li>
-          <li><input type="button" value="delete"></input></li>
+          <li><a href="/create" onClck={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('create');
+          }.bind(this)}>create</a></li>
+          <li><a href="/update" onClck={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('update');
+          }.bind(this)}>update</a></li>
+          <li><input type="button" value="delete" onClck={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('delete');
+          }.bind(this)}></input></li>
         </ul>
       );
     }
